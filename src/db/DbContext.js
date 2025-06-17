391d150cb1046b53f3b761fbcc3bd83e560970a7
Jeremy Fowler <jembyfowler@gmail.com>
2025-06-17 10:06:48 -0600

    registered schema with database

--- src/db/DbContext.js
+++ src/db/DbContext.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account.js'
 import { ValueSchema } from '../models/Value.js'
+import { CarSchema } from '../models/Car.js';
 
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
+  Cars = mongoose.model('Car', CarSchema)
 }
 
 export const dbContext = new DbContext()
