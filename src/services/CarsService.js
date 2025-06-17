05c0fa9d1c211aaf0f47a4a2e76939d647829834
Jeremy Fowler <jembyfowler@gmail.com>
2025-06-17 12:09:35 -0600

    real pagination 😎

--- src/services/CarsService.js
+++ src/services/CarsService.js
@@ -1,3 +1,4 @@
+import { BadRequest } from "@bcwdev/auth0provider/lib/Errors.js"
 import { dbContext } from "../db/DbContext.js"
 
 class CarsService {
@@ -7,7 +8,7 @@ class CarsService {
   }
 
   async getCarsByQuery(carQuery) {
-    const pageNumber = carQuery.page || 1
+    const pageNumber = parseInt(carQuery.page) || 1
     delete carQuery.page
 
     const carLimit = 10
@@ -16,13 +17,32 @@ class CarsService {
     // const cars = await dbContext.Cars.find({ make: 'Mazda', model: 'Miata' })
     // const cars = await dbContext.Cars.find(carQuery).populate('creator', '-subs -email')
     // const cars = await dbContext.Cars.find(carQuery).populate('creator', ['name', 'picture'])
+
+    const carsCount = await dbContext.Cars.countDocuments(carQuery)
+    const totalPages = Math.ceil(carsCount / carLimit)
+
+    if (pageNumber > totalPages) {
+      throw new BadRequest(`${pageNumber} is larger than the maximum amount of pages (${totalPages})`)
+    }
+
+
     const cars = await dbContext.Cars
       .find(carQuery)
       .skip(skipAmount)
       .limit(carLimit)
       .populate('creator', 'name picture')
 
-    return cars
+
+    const pageResponse = {
+      currentPage: pageNumber,
+      previousPage: pageNumber - 1 || null,
+      // NOTE goofy code below
+      nextPage: totalPages == pageNumber ? null : pageNumber + 1,
+      totalResults: carsCount,
+      totalPages: totalPages,
+      cars: cars
+    }
+    return pageResponse
   }
 }
 
