2c56629d03e160155f99747be25b63d0cc2017be
Jeremy Fowler <jembyfowler@gmail.com>
2025-06-17 11:48:23 -0600

    pagination

--- src/services/CarsService.js
+++ src/services/CarsService.js
@@ -7,10 +7,21 @@ class CarsService {
   }
 
   async getCarsByQuery(carQuery) {
+    const pageNumber = carQuery.page || 1
+    delete carQuery.page
+
+    const carLimit = 10
+    const skipAmount = (pageNumber - 1) * carLimit
+
     // const cars = await dbContext.Cars.find({ make: 'Mazda', model: 'Miata' })
     // const cars = await dbContext.Cars.find(carQuery).populate('creator', '-subs -email')
     // const cars = await dbContext.Cars.find(carQuery).populate('creator', ['name', 'picture'])
-    const cars = await dbContext.Cars.find(carQuery).populate('creator', 'name picture')
+    const cars = await dbContext.Cars
+      .find(carQuery)
+      .skip(skipAmount)
+      .limit(carLimit)
+      .populate('creator', 'name picture')
+
     return cars
   }
 }
