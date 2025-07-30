030bccb4efc549b73f500dd9ebf12bb7837c2cda
stittpaul2001 <stittpaul2001@gmail.com>
2025-07-30 16:35:37 -0600

    getting started

--- www/env.js
+++ www/env.js
@@ -1,3 +1,3 @@
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-oxp3h7skmjsnm3i2.us.auth0.com'
+export const clientId = 'VJybAHyXyIO25zTxU1wbavVcnXBSW97j'
+export const audience = 'htttp://Wagonhammer.com'
