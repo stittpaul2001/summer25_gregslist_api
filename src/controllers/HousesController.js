e0248f0ae3d21f0264e67f54a6a3fdc123ee2d16
stittpaul2001 <stittpaul2001@gmail.com>
2025-08-04 12:23:03 -0600

    got properties from figma added to my schema

--- src/controllers/HousesController.js
+++ src/controllers/HousesController.js
@@ -1,7 +1,28 @@
+import { housesService } from "../services/HousesService.js";
 import BaseController from "../utils/BaseController.js";
 
 export class HousesController extends BaseController {
   constructor() {
-    super('api/houses');
+    super('api/houses')
+    this.router
+      .get('', this.getAllHouses)
   }
+
+  /**
+ * @param {import("express").Request} request,
+ * @param {import("express").Response} response,
+ * @param {import("express").NextFunction} next,
+ */
+
+  async getAllHouses(request, response, next) {
+    try {
+      const houses = await housesService.getAllHouses()
+      response.send(houses)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+
+
 }
--- src/models/House.js
+++ src/models/House.js
@@ -1,9 +1,81 @@
 import { Schema } from "mongoose";
 
+function validateDate(value) {
+  const currentYear = new Date().getFullYear()
+  return value <= currentYear + 1
+}
+
 // House.js
 export const HouseSchema = new Schema(
   {
     // Properties from UML go here!
+    bedrooms: {
+      type: Number,
+      min: 0,
+      max: 30,
+      required: true
+    },
+
+    bathrooms: {
+      type: Number,
+      min: 0,
+      max: 25,
+      required: true
+    },
+
+    levels: {
+      type: Number,
+      min: 1,
+      max: 4,
+      required: true
+    },
+
+    price: {
+      type: Number,
+      min: 0,
+      max: 10000000,
+      required: true
+    },
+
+    imgUrl: {
+      type: String,
+      maxlength: 500,
+      required: true
+    },
+
+    description: {
+      type: String,
+      maxlength: 500
+    },
+
+    year: {
+      type: Number,
+      min: 1000,
+      required: true,
+      validate: {
+        validator: validateDate,
+        message: `{Value} is not a valid year (must be less than or equal to ${new Date().getFullYear() + 1})`
+      }
+    },
+
+    creatorId: {
+      type: Schema.ObjectId,
+      required: true,
+      ref: 'Account'
+    }
   },
-  { timestamps: true, toJSON: { virtuals: true } }
-);
+
+  {
+    timestamps: true,
+    versionKey: false,
+    toJSON: { virtuals: true }
+  }
+
+)
+
+HouseSchema.virtual('creator', {
+  localField: 'creatorId',
+  ref: 'Account',
+  foreignField: '_id',
+  justOne: true
+})
